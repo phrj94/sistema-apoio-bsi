ef3daa0907f2c97b88f6eb295758e8b35e6c953a
phrj94 <ph.rj.94@hotmail.com>
2023-06-08 09:55:52 -0300

    Retorna período em que a disciplina foi cursada

--- backend/services/handlerPDFContent.js
+++ backend/services/handlerPDFContent.js
@@ -7,39 +7,42 @@ const regexSituacao = new RegExp(/(aprovado.*|reprovado.*|matrícula.*|trancamen
 const regexTrancamento = new RegExp(/(trancamento.*)$/gmi);
 //Melhorar, casos de nomes de disciplina muito grandes e que acabam quebrando em duas linhas não estão sendo contemplados.
 const regexNomeDisciplina = new RegExp(/[a-zA-Z]{3}[0-9]{4}([A-Za-záãÁÀÃÂÉÈÊÍÌÎÓÒÕÔÚÙÛÇ\s]*)/gmi);
-
+const regexPegaPeriodo = new RegExp(/^(((([0-9])°). semestre de )[0-9]{4})/gmi);
 const criaVetorDisciplinas = (disciplinas) => {
-   
+
     const vetorDisciplinas = disciplinas.map(disciplina => ({
-        codigo: disciplina.match(regexDisciplina) && disciplina.match(regexDisciplina).toString(),
-        situacao: disciplina.match(regexSituacao) && disciplina.match(regexSituacao).toString(),
-        trancamento: disciplina.match(regexTrancamento) && disciplina.match(regexTrancamento).toString(),
-        nome: disciplina.match(regexNomeDisciplina).toString().split(regexDisciplina)[1]
+        codigo: disciplina.disciplina.match(regexDisciplina) && disciplina.disciplina.match(regexDisciplina).toString(),
+        situacao: disciplina.disciplina.match(regexSituacao) && disciplina.disciplina.match(regexSituacao).toString(),
+        trancamento: disciplina.disciplina.match(regexTrancamento) && disciplina.disciplina.match(regexTrancamento).toString(),
+        nome: disciplina.disciplina.match(regexNomeDisciplina).toString().split(regexDisciplina)[1],
+        periodo: disciplina.periodo
     }))
-   
+
     return vetorDisciplinas;
 }
 
 const readPdf = async (uri) => {
-    
+
     const buffer = fs.readFileSync(path.resolve(__dirname, "../" + uri));
-    
+
     try {
         const data = await pdfParse(buffer);
-        
-        const splitted = data.text.split('\n')
 
-        // console.log(splitted[20])
+        const splitted = data.text.split('\n')
+        let periodo = "";
+
         let disciplinas = [];
         for (let i = 0; i < splitted.length; i++) {
             // console.log(splitted[i], i)
             if (regexTrancamento.test(splitted[i])) {
                 continue;
             }
-             
+
+            if (regexPegaPeriodo.test(splitted[i])) periodo = splitted[i];
+
             if (regexDisciplina.test(splitted[i]) && regexSituacao.test(splitted[i])) {
-               
-                disciplinas.push(splitted[i]);
+
+                disciplinas.push({ disciplina: splitted[i], periodo });
                 continue;
             }
 
@@ -50,10 +53,10 @@ const readPdf = async (uri) => {
                     disciplina += splitted[j];
                     j++;
                 } while (!regexSituacao.test(splitted[j - 1]));
-                disciplinas.push(disciplina);
+                disciplinas.push({ disciplina, periodo });
             }
         }
-       
+
         return criaVetorDisciplinas(disciplinas);
     } catch (err) {
         throw new Error(err);
